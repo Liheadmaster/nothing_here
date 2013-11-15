223dc63ac040af97a10a49d70504f843aaed804a
Li He <lihe@admaster.com.cn>
2013-11-15 10:21:27 +0800

    完成PA5 A-part2

--- coursera/Introduction to Recommender Systems /ii-assignment/src/main/java/edu/umn/cs/recsys/ii/SimpleGlobalItemScorer.java
+++ coursera/Introduction to Recommender Systems /ii-assignment/src/main/java/edu/umn/cs/recsys/ii/SimpleGlobalItemScorer.java
@@ -3,10 +3,13 @@ package edu.umn.cs.recsys.ii;
 import org.grouplens.lenskit.basic.AbstractGlobalItemScorer;
 import org.grouplens.lenskit.scored.ScoredId;
 import org.grouplens.lenskit.vectors.MutableSparseVector;
+import org.grouplens.lenskit.vectors.VectorEntry;
 
 import javax.annotation.Nonnull;
 import javax.inject.Inject;
+
 import java.util.Collection;
+import java.util.HashMap;
 
 /**
  * Global item scorer to find similar items.
@@ -29,8 +32,22 @@ public class SimpleGlobalItemScorer extends AbstractGlobalItemScorer {
     @Override
     public void globalScore(@Nonnull Collection<Long> items, @Nonnull MutableSparseVector scores) {
         scores.fill(0);
+        
         // TODO score items in the domain of scores
         // each item's score is the sum of its similarity to each item in items, if they are
         // neighbors in the model.
+        for (VectorEntry ent : scores){
+        	long item = ent.getKey();
+        	HashMap mappedNeighbor = new HashMap();
+            for (ScoredId _sid: model.getNeighbors(item)){
+            	mappedNeighbor.put(_sid.getId(), _sid.getScore());
+            }
+            double _score = 0.0;
+        	for (long compareItem : items){
+        		if (item == compareItem || !mappedNeighbor.containsKey(compareItem)) continue;
+        		_score += (Double)mappedNeighbor.get(compareItem);
+        	}
+        	scores.set(item, _score);
+        }
     }
 }
